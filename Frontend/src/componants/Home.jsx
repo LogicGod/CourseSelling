e4d0d4f800721f2e3c670d50484c3911c2b89268
LogicGod <logiclionhelloworld@gmail.com>
2025-03-20 15:32:53 +0530

    add card slider

--- Frontend/src/componants/Home.jsx
+++ Frontend/src/componants/Home.jsx
@@ -5,7 +5,16 @@ import Footer from './Footer';
 function Home() {
 
     const [Data1 , setData1] = useState([]); 
+    const[index,setIndex]=useState(0)
+    const[images,setImages]=useState(["https://appxcontent.kaxa.in/subject/2024-07-07-0.9522250790418232.png","https://appxcontent.kaxa.in/subject/2024-07-05-0.3715048534115637.jpeg","https://appxcontent.kaxa.in/subject/2024-07-05-0.8025085370209641.jpeg","https://appxcontent.kaxa.in/subject/2024-07-05-0.3715048534115637.jpeg"])
 
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex >=3 ? 0 : prevIndex + 1));
+        }, 7000);
+    
+        return () => clearInterval(intervalId); // Clear the interval on unmount
+    }, []);
       useEffect(() => {       
     const getData = async() =>
         {
@@ -29,8 +38,13 @@ function Home() {
   return (
     <div className='  h-auto  flex justify-center items-center py-4 md:ml-[20vw]'>
     <div className='flex flex-col gap-8 '>
-    <div className='h-auto w-[95vw]  mt-[80px] rounded-xl overflow-hidden md:w-[60vw] lg:w-[70vw] md:ml-[10vw] lg:ml-[1vw] '>
-        <img src="https://appxcontent.kaxa.in/subject/2024-07-07-0.9522250790418232.png" className='object-cover '/>
+    <div className='h-auto w-[95vw]  mt-[80px] rounded-xl overflow-hidden md:w-[60vw] lg:w-[70vw] md:ml-[10vw] lg:ml-[1vw]  '>
+        <img src={images[index]} className={`object-cover  transform: translateX(-${index * 100}%) transition: transform 0.5s ease-in-outtransform: translateX(-${index * 100}%) transition: transform 0.5s ease-in-out`}/>
+        <div className=' flex  justify-center gap-6 pt-4 cursor-pointer '>
+        <button className='bg-gray-500 cursor-pointer hover:bg-black p-1 rounded-full' onClick={()=> setIndex(0)}></button>
+        <button className='bg-gray-500 cursor-pointer hover:bg-black p-1 rounded-full' onClick={()=> setIndex(1)}></button>
+        <button className='bg-gray-500 cursor-pointer hover:bg-black p-1 rounded-full' onClick={()=> setIndex(2)}></button>
+        </div>
         
     </div>
     <div className='  '>
@@ -38,7 +52,7 @@ function Home() {
     </div>
     <div className='max-lg:flex-col flex max-md:gap- md:flex md:flex-wrap lg:gap-1 md:w-full max-lg:items-center max-lg:gap-8 lg:ml-[0vw] md:ml-[5vw]'>
         { Data1 && Data1.map((item)=>(
-            <div className='h-auto lg:h-[30vw] w-[95vw]  bg-white rounded-lg overflow-hidden border-2 border-gray-400 md:w-[50vw] lg:w-[23vw]  '>
+            <div className='h-auto  w-[95vw]  bg-white rounded-lg overflow-hidden border-2 border-gray-400 md:w-[50vw] lg:w-[23vw]  '>
                 <img src={item.imageurl} className='h-[55%] w-[100%]' />
                 <div className='p-4 flex flex-col gap-4'>
                     <div className='md:h-[65px]'>
